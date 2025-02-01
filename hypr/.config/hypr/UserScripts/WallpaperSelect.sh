a3020266346ccaf6a278b9d35e3faa544d667827
yjy <3253932102@qq.com>
2025-02-01 23:20:54 +0800

    🎨 Perf: hyprland wallpaper

--- hypr/.config/hypr/UserScripts/WallpaperSelect.sh
+++ hypr/.config/hypr/UserScripts/WallpaperSelect.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */ 
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */
 # This script for selecting wallpapers (SUPER W)
 
 # WALLPAPERS PATH
-wallDIR="$HOME/Pictures/wallpapers"
+wallDIR="$HOME/Pictures/wallpapers/gruvbox/"
 SCRIPTSDIR="$HOME/.config/hypr/scripts"
 
 # variables
@@ -16,7 +16,7 @@ BEZIER=".43,1.19,1,.4"
 SWWW_PARAMS="--transition-fps $FPS --transition-type $TYPE --transition-duration $DURATION"
 
 # Check if swaybg is running
-if pidof swaybg > /dev/null; then
+if pidof swaybg >/dev/null; then
   pkill swaybg
 fi
 
@@ -33,13 +33,13 @@ rofi_command="rofi -i -show -dmenu -config ~/.config/rofi/config-wallpaper.rasi"
 menu() {
   # Sort the PICS array
   IFS=$'\n' sorted_options=($(sort <<<"${PICS[*]}"))
-  
+
   # Place ". random" at the beginning with the random picture as an icon
   printf "%s\x00icon\x1f%s\n" "$RANDOM_PIC_NAME" "$RANDOM_PIC"
-  
+
   for pic_path in "${sorted_options[@]}"; do
     pic_name=$(basename "$pic_path")
-    
+
     # Displaying .gif to indicate animated images
     if [[ ! "$pic_name" =~ \.gif$ ]]; then
       printf "%s\x00icon\x1f%s\n" "$(echo "$pic_name" | cut -d. -f1)" "$pic_path"
@@ -55,7 +55,7 @@ swww query || swww-daemon --format xrgb
 # Choice of wallpapers
 main() {
   choice=$(menu | $rofi_command)
-  
+
   # Trim any potential whitespace or hidden characters
   choice=$(echo "$choice" | xargs)
   RANDOM_PIC_NAME=$(echo "$RANDOM_PIC_NAME" | xargs)
@@ -68,7 +68,7 @@ main() {
 
   # Random choice case
   if [[ "$choice" == "$RANDOM_PIC_NAME" ]]; then
-	swww img -o "$focused_monitor" "$RANDOM_PIC" $SWWW_PARAMS;
+    swww img -o "$focused_monitor" "$RANDOM_PIC" $SWWW_PARAMS
     sleep 0.5
     "$SCRIPTSDIR/WallustSwww.sh"
     sleep 0.2
@@ -95,9 +95,9 @@ main() {
 }
 
 # Check if rofi is already running
-if pidof rofi > /dev/null; then
+if pidof rofi >/dev/null; then
   pkill rofi
-  sleep 1  # Allow some time for rofi to close
+  sleep 1 # Allow some time for rofi to close
 fi
 
 main
@@ -107,4 +107,3 @@ sleep 0.5
 
 sleep 0.2
 "$SCRIPTSDIR/Refresh.sh"
-
